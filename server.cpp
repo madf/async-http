1784a7bfd1bad34320126049c285aa5305fbecce
Elena Mamontova <helenh463@gmail.com>
2020-07-20 11:16:02 +0300

    The reference() function added to server.cpp file.

--- server.cpp
+++ server.cpp
@@ -5,6 +5,18 @@
 
 using boost::asio::ip::tcp;
 
+void reference()
+{
+    std::cout << "Usage:\n";
+    std::cout << "server [-a/--address <bind-address>][-d/--dir <work-dir>][-o/--out-log <log-file-name>][-h/--help][-v/--version]\n\n";
+
+    std::cout << "-d, --dir <work-dir> - specifies the working directory with files for the server;\n";
+    std::cout << "-o, --out-log <log-file-name> - specifies the file name for logging for the server;\n";
+    std::cout << "-a, --address <bind-address> - can be specified in <address>[:<port>] form, where <address> can be IP-address or domain name, <port> - number;\n";
+    std::cout << "-v, --version - server version;\n";
+    std::cout << "-h, --help - show this text.\n";
+}
+
 std::string make_daytime_string()
 {
     char buffer[80];
