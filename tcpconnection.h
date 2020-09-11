35440f71e8935ccc0e1a8ac75df164eb3dffd3ce
Elena Mamontova <helenh463@gmail.com>
2020-09-11 13:26:33 +0300

    The tcpconnection.h new file added.

---
+++ tcpconnection.h
@@ -0,0 +1,25 @@
+#ifndef TCPCONNECTION_H
+#define TCPCONNECTION_H
+
+#include <boost/asio.hpp>
+#include <string>
+
+class Tcpconnection
+{
+    public:
+        Tcpconnection(boost::asio::io_service& io_service);
+
+        void start();
+        boost::asio::ip::tcp::socket& socket();
+
+    private:
+
+        void handle_read(const boost::system::error_code& error,
+          size_t /*bytes_transferred*/);
+
+        boost::asio::ip::tcp::socket socket_;
+        char buff_[1024];
+        std::string message_;
+};
+
+#endif
