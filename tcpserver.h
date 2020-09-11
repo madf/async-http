2872d0e06869d58204986bb0033b5a8fa1cda14d
Elena Mamontova <helenh463@gmail.com>
2020-09-11 13:52:45 +0300

    The header file "tcpconnection" added. The handle_write() method removed. The socket parameter replaced by connection in the handle_accept() method. The class member variable char buff added.

--- tcpserver.h
+++ tcpserver.h
@@ -1,6 +1,7 @@
 #ifndef TCPSERVER_H
 #define TCPSERVER_H
 
+#include "tcpconnection.h"
 #include <boost/asio.hpp>
 #include <boost/shared_ptr.hpp>
 #include <string>
@@ -14,8 +15,7 @@ class Tcpserver
         std::string make_daytime_string();
         std::string make_log_line(const std::string& message);
         void write_log(const std::string& message);
-        void handle_write(const boost::system::error_code& error, size_t /*bytes_transferred*/);
-        void handle_accept(boost::shared_ptr<boost::asio::ip::tcp::socket> socket, const boost::system::error_code& error);
+        void handle_accept(boost::shared_ptr<Tcpconnection> connection, const boost::system::error_code& error);
         void start_accept();
         void handle_resolve(const boost::system::error_code& err, boost::asio::ip::tcp::resolver::iterator endpoint_iterator);
 
@@ -23,6 +23,7 @@ class Tcpserver
         boost::asio::ip::tcp::resolver resolver_;
         boost::asio::ip::tcp::acceptor acceptor_;
         std::string& outfile_;
+        char buff_[1024];
 };
 
 #endif
