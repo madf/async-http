84291b4fda3f3c9f0040c0072408140fb67a410f
Elena Mamontova <helenh463@gmail.com>
2020-09-22 17:25:47 +0300

    The boost::shared_ptr parameter changed to std::shared_ptr in the handle_accept() method.

--- tcpserver.h
+++ tcpserver.h
@@ -3,7 +3,6 @@
 
 #include "tcpconnection.h"
 #include <boost/asio.hpp>
-#include <boost/shared_ptr.hpp>
 #include <string>
 
 class Tcpserver
@@ -15,7 +14,7 @@ class Tcpserver
         std::string make_daytime_string();
         std::string make_log_line(const std::string& message);
         void write_log(const std::string& message);
-        void handle_accept(boost::shared_ptr<Tcpconnection> connection, const boost::system::error_code& error);
+        void handle_accept(std::shared_ptr<Tcpconnection> connection, const boost::system::error_code& error);
         void start_accept();
         void handle_resolve(const boost::system::error_code& err, boost::asio::ip::tcp::resolver::iterator endpoint_iterator);
 
