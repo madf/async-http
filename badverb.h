c5fa4ac48a135227f52f3eb5fb70eb7d5de58455
Elena Mamontova <helenh463@gmail.com>
2021-01-05 15:14:30 +0200

    The class name Getverb changed to GetVerb in getverb.h file.

--- badverb.h
+++ badverb.h
@@ -4,7 +4,7 @@
 #include <stdexcept>
 #include <string>
 
-class Badverb: public std::runtime_error
+class BadVerb: public std::runtime_error
 {
     public:
         using std::runtime_error::runtime_error;
