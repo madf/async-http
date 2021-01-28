542fc3060111b8f53e69e05c6084bdf1d9749a10
Elena Mamontova <helenh463@gmail.com>
2021-01-28 15:23:02 +0200

    <boost/asio.hpp> header file and overloaded function write_log() added in log.h file.

--- log.h
+++ log.h
@@ -1,8 +1,10 @@
 #ifndef LOG_H
 #define LOG_H
 
+#include <boost/asio.hpp>
 #include <string>
 
 void write_log(const std::string& message, const std::string& outfile);
+void write_log(const boost::asio::ip::tcp::socket& socket, const std::string& message, const std::string& outfile);
 
 #endif
