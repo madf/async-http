e03c89409afb919871fc67997ebbf005b591ae2d
Elena Mamontova <helenh463@gmail.com>
2021-02-01 13:17:27 +0200

    The 'const' added to variable time_t now.

--- log.cpp
+++ log.cpp
@@ -6,7 +6,7 @@
 std::string make_daytime_string()
 {
     char buffer[80];
-    time_t now = time(nullptr);
+    const time_t now = time(nullptr);
     struct tm* timeinfo = localtime(&now);
     strftime(buffer, 80, "%Y-%m-%d %H:%M:%S", timeinfo);
     return buffer;
