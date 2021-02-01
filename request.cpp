83332c72d1911457bc4f5985777a6fbb5d02a78d
Elena Mamontova <helenh463@gmail.com>
2021-02-01 15:31:37 +0200

    Messages in throw BadRequest() changed to more detailed.

--- request.cpp
+++ request.cpp
@@ -5,13 +5,13 @@ Request::Request(const std::string& start_str)
 {
     const size_t pos_space_first = start_str.find_first_of(' ');
     if (pos_space_first == std::string::npos)
-        throw BadRequest("Bad request.");
+        throw BadRequest("There is no space after the verb and before the version in the request.");
 
     m_verb = start_str.substr(0, pos_space_first);
 
     const size_t pos_space_second = start_str.find_last_of(' ');
     if (pos_space_second == std::string::npos)
-        throw BadRequest("Bad request.");
+        throw BadRequest("There is no space after the verb and before the version in the request.");
 
     m_path = start_str.substr(pos_space_first + 1, pos_space_second - pos_space_first - 1);
     m_version = start_str.substr(pos_space_second + 1);
